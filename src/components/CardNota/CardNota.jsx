11acf5f733c034ee8efe9aa9660d4a9ba6e222be
Paul Richard <paulrpm@gmail.com>
2023-01-06 15:56:28 -0300

    added deleting function

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import {ReactComponent as DeleteIcon} from "../../assets/delete.svg";
 
 class CardNota extends Component {
+
+    _noteDelete(){
+        this.props.noteDelete(this.props.index)
+    }
 
     render() {
         return (
@@ -9,6 +14,7 @@ class CardNota extends Component {
                 <header>
                     <h5>{this.props.group}</h5>
                     <h3 className="card-nota-title">{this.props.title}</h3>
+                    <DeleteIcon onClick={this._noteDelete.bind(this)}/>
                 </header>
                 <p>
                     {this.props.text}
